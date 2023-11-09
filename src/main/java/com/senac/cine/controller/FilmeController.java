ae95a4ef24c2be0f4bf66fd4c018708aac2b5c5d
eclipseCJP <cris.tecwordd@gmail.com>
2023-11-08 21:05:10 -0400

    pagina de cadastro e lista de filmes

--- src/main/java/com/senac/cine/controller/FilmeController.java
+++ src/main/java/com/senac/cine/controller/FilmeController.java
@@ -1,11 +1,13 @@
 
 package com.senac.cine.controller;
-
-
 import com.senac.cine.model.Filme;
+import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -14,10 +16,42 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FilmeController {
+    ArrayList<Filme> filmes = new ArrayList<>();
     
     @GetMapping("/cadastro-filmes")
     public String exibeFormulario(Model model){
         model.addAttribute("filme", new Filme());
         return "cadastro-filmes";
     }
+    /*
+    @GetMapping("/lista-filmes")
+    public String getListaFilmes(Model model){
+            model.addAttribute("filmes", filmes);
+            return "registro";
+        }
+    */
+    @PostMapping("/cadastro-filme")
+    public String setFilme(Model model,@RequestParam int id,@RequestParam String titulo,@RequestParam String sinopse,@RequestParam String genero, @RequestParam int anoLancamento){
+        Filme filme = new Filme();
+        filme.setId(id);
+        filme.setTitulo(titulo);
+
+            filme.setGenero(genero);
+
+        filme.setAnoLancamento(anoLancamento);
+        filmes.add(filme);
+        model.addAttribute("filme",filme);
+        return "registro";
+    }
+        @PostMapping("/registro")
+        public String exibeCadastro(@ModelAttribute Filme filme, Model model){
+           filmes.add(filme);
+            model.addAttribute("filmes", filmes);
+            return "registro";
+        }
+        
+        
+       
+        
+        
 }
